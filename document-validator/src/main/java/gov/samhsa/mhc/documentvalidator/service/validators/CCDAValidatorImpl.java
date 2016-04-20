a317d5e22197cad0f9e66fb075862fb884bc6976
JiahaoLi <Jiahao.Li@feisystems.com>
2016-04-20 11:00:03 -0400

    Add PostConstruct annotation on buildStaticPackageRegistration method
    #8610 - As patient, I can view and provide a consent for a CCD, which can be segmented by MHC

--- document-validator/src/main/java/gov/samhsa/mhc/documentvalidator/service/validators/CCDAValidatorImpl.java
+++ document-validator/src/main/java/gov/samhsa/mhc/documentvalidator/service/validators/CCDAValidatorImpl.java
@@ -1,28 +1,3 @@
-/**
- * Copyright (c) 2015, Standards Implementation & Testing Environment (SITE)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package gov.samhsa.mhc.documentvalidator.service.validators;
 
 import gov.samhsa.mhc.documentvalidator.service.dto.DiagnosticType;
@@ -35,6 +10,7 @@ import org.openhealthtools.mdht.uml.cda.util.ValidationResult;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
 
+import javax.annotation.PostConstruct;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -42,21 +18,24 @@ import java.util.stream.Collectors;
 @Service
 public class CCDAValidatorImpl implements CCDAValidator {
 
+    @PostConstruct
+    public void buildStaticPackageRegistration() {
+        ConsolPackage.eINSTANCE.eClass();
+    }
+
     @Override
     public ArrayList<DocumentValidationResult> validateCCDA(InputStream ccdaFile) throws SAXException {
+
+        // validate on load
+        // create validation result to hold diagnostics
         ValidationResult result = new ValidationResult();
-        createValidationResultObjectToCollectDiagnosticsProducedDuringValidation();
+
         try {
             CDAUtil.load(ccdaFile, result);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return processValidationResults(result);
-    }
-
-    private void createValidationResultObjectToCollectDiagnosticsProducedDuringValidation() {
-        ConsolPackage.eINSTANCE.eClass();
     }
 
     private ArrayList<DocumentValidationResult> processValidationResults(ValidationResult result) {
