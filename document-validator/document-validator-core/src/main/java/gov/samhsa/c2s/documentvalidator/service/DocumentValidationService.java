624fdea5fa9cfb71fcf28d8046d2ed33e2b29925
JiahaoLi <Jiahao.Li@feisystems.com>
2017-05-11 23:30:58 -0400

    Refactor to create a new spring boot project
    #13489:As a developer, I will have integrated document validator into v3.0

--- document-validator/document-validator-core/src/main/java/gov/samhsa/c2s/documentvalidator/service/DocumentValidationService.java
+++
@@ -1,8 +0,0 @@
-package gov.samhsa.c2s.documentvalidator.service;
-
-import gov.samhsa.c2s.documentvalidator.service.dto.ValidationRequestDto;
-import gov.samhsa.c2s.documentvalidator.service.dto.ValidationResponseDto;
-
-public interface DocumentValidationService {
-    ValidationResponseDto validateDocument(ValidationRequestDto requestDto);
-}
