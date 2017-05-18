92e2ec72550221de4b24e1692c8c9afc396e8bcd
JiahaoLi <Jiahao.Li@feisystems.com>
2017-05-18 19:15:14 -0400

    Change to user external servlet container to deploy application
    #13489:As a developer, I will have integrated document validator into v3.0

--- document-validator/src/main/java/gov/samhsa/c2s/documentvalidator/DocumentValidatorApplication.java
+++ document-validator/src/main/java/gov/samhsa/c2s/documentvalidator/DocumentValidatorApplication.java
@@ -2,15 +2,21 @@ package gov.samhsa.c2s.documentvalidator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class DocumentValidatorApplication {
-
+public class DocumentValidatorApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DocumentValidatorApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DocumentValidatorApplication.class);
     }
 }
