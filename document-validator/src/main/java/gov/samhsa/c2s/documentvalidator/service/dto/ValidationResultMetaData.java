f6b4d60d9dcce90fc61b16da21f873818b188a9a
JiahaoLi <Jiahao.Li@feisystems.com>
2017-05-19 00:28:09 -0400

    Refactor ValidationResultMetaData constructor
    #13489:As a developer, I will have integrated document validator into v3.0

--- document-validator/src/main/java/gov/samhsa/c2s/documentvalidator/service/dto/ValidationResultMetaData.java
+++ document-validator/src/main/java/gov/samhsa/c2s/documentvalidator/service/dto/ValidationResultMetaData.java
@@ -2,6 +2,7 @@ package gov.samhsa.c2s.documentvalidator.service.dto;
 
 import gov.samhsa.c2s.documentvalidator.service.schema.ValidationDiagnosticType;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,9 +11,9 @@ public class ValidationResultMetaData {
     private final Map<String, AtomicInteger> validationSummaryMap = new LinkedHashMap<>();
 
     public ValidationResultMetaData() {
-        for (ValidationDiagnosticType diagnosticType : ValidationDiagnosticType.values()) {
-            validationSummaryMap.put(diagnosticType.getTypeName(), new AtomicInteger(0));
-        }
+        Arrays.stream(ValidationDiagnosticType.values())
+                .forEach(diagnosticType -> validationSummaryMap
+                        .put(diagnosticType.getTypeName(), new AtomicInteger(0)));
     }
 
     public Map<String, AtomicInteger> getValidationSummaryMap() {
