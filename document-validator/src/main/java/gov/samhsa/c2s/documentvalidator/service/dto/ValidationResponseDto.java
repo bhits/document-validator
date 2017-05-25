4e248e33affc4cb3debbe23df6565b815bcf6b98
JiahaoLi <Jiahao.Li@feisystems.com>
2017-05-25 12:03:45 -0400

    Add default active docker profile in Dockerfile
    #13489:As a developer, I will have integrated document validator into v3.0

--- document-validator/src/main/java/gov/samhsa/c2s/documentvalidator/service/dto/ValidationResponseDto.java
+++ document-validator/src/main/java/gov/samhsa/c2s/documentvalidator/service/dto/ValidationResponseDto.java
@@ -1,5 +1,6 @@
 package gov.samhsa.c2s.documentvalidator.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import gov.samhsa.c2s.documentvalidator.service.schema.DocumentType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,5 +17,6 @@ public class ValidationResponseDto {
     private DocumentType documentType;
     private boolean isDocumentValid;
     private DocumentValidationResultSummary validationResultSummary;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private List<DocumentValidationResultDetail> validationResultDetails;
 }
